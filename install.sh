aeda26bb6fb12f141efcee48d4c79a122722d80c
Rebeca Rguez <72274516+rrrguez@users.noreply.github.com>
2024-09-26 14:55:30 +0000

    Modificado install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-aliases
+# Asegurarse de que el archivo .aliases existe y se puede cargar
+if [ -f /workspaces/.codespaces/.persistedshare/dotfiles/.aliases ]; then
+    echo "Sourcing aliases from dotfiles"
+    grep -qxF 'source /workspaces/.codespaces/.persistedshare/dotfiles/.aliases' ~/.bashrc || echo 'source /workspaces/.codespaces/.persistedshare/dotfiles/.aliases' >> ~/.bashrc
+fi
