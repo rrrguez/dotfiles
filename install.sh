cb5cefdde74b6cc1f893d2da00ad6181438fac1b
Rebeca Rguez <72274516+rrrguez@users.noreply.github.com>
2024-09-27 15:27:33 +0100

    Prueba 4

--- install.sh
+++ install.sh
@@ -6,14 +6,7 @@ echo "Ejecutando install.sh..."
 # Verificar si el archivo .aliases existe
 if [ -f /workspaces/.codespaces/.persistedshare/dotfiles/.aliases ]; then
     echo "Archivo .aliases encontrado, añadiendo a .bashrc"
-    
-    # Comprobar si la línea ya existe en .bashrc
-    if grep -qxF 'source /workspaces/.codespaces/.persistedshare/dotfiles/.aliases' ~/.bashrc; then
-        echo "La fuente del archivo .aliases ya está en .bashrc"
-    else
-        echo "Añadiendo fuente de .aliases a .bashrc"
-        echo 'source /workspaces/.codespaces/.persistedshare/dotfiles/.aliases' >> ~/.bashrc
-    fi
+    echo 'source /workspaces/.codespaces/.persistedshare/dotfiles/.aliases' >> ~/.bashrc
     source ~/.bashrc
 else
     echo "Error: Archivo .aliases no encontrado"
