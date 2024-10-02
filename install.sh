a77f2ae4ce5003acf7ef9ae4ea05a7267ec74041
Rebeca Rguez <72274516+rrrguez@users.noreply.github.com>
2024-10-02 16:37:13 +0100

    Ahora los mensajes se almacenan en un archivo install.log

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Mensaje de inicio para saber si se está ejecutando
-echo "Ejecutando install.sh..."
+echo "Ejecutando install.sh..." >> /workspaces/.codespaces/.persistedshare/install.log
 
 # Verificar si el archivo .aliases existe
 if [ -f /workspaces/.codespaces/.persistedshare/dotfiles/.aliases ]; then
-    echo "Archivo .aliases encontrado, añadiendo a .bashrc"
+    echo "Archivo .aliases encontrado, añadiendo a .bashrc" >> /workspaces/.codespaces/.persistedshare/install.log
     echo 'source /workspaces/.codespaces/.persistedshare/dotfiles/.aliases' >> ~/.bashrc
     source ~/.bashrc
 else
-    echo "Error: Archivo .aliases no encontrado"
+    echo "Error: Archivo .aliases no encontrado" >> /workspaces/.codespaces/.persistedshare/install.log
 fi
